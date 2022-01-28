b8c7248f8f46663f17172c0adff98f3f1569636c
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-01-28 12:18:03 -0300

    requisito 02 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,44 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import React from 'react';
-import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router-dom';
-import App from '../App';
-
-test('Teste se a página contém as informações sobre a Pokédex.', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-
-  const clickAbout = screen.getByRole('link', { name: /About/i });
-  userEvent.click(clickAbout);
-
-  expect(clickAbout).toBeInTheDocument();
-});
+/* import userEvent from '@testing-library/user-event'; */
+import renderWithRouter from '../renderWithRouter';
+/* import { MemoryRouter } from 'react-router-dom'; */
+import About from '../components/About';
 
 // testa h2
 test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  renderWithRouter(<About />);
 
-  const aboutPokedex = screen.getByRole('heading', { level: 2 });
+  const aboutPokedex = screen.getByRole('heading', { name: /About Pokédex/i });
   expect(aboutPokedex).toBeInTheDocument();
 });
 
 // testa parágrafo
 test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const clickAbout = screen.getByRole('link', { name: /About/i });
-  userEvent.click(clickAbout);
-
+  renderWithRouter(<About />);
   const firstPragraph = screen.getByText(/This application simulates a Pokédex/i);
   const secondParagraph = screen.getByText(/One can filter Pokémons by type/i);
 
@@ -47,13 +24,9 @@ test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', 
 });
 
 test('testa se renderiza a imagem', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-
-  const takeImage = screen.getByRole('img',
-    { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.pngPikachu crying because the page requested was not found' });
+  renderWithRouter(<About />);
+  const takeImage = screen.getByAltText(/Pokédex/i);
   expect(takeImage).toBeInTheDocument();
+  expect(takeImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  expect(takeImage).toHaveAttribute('alt', 'Pokédex');
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+/* import userEvent from '@testing-library/user-event'; */
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+/* test('Teste se página contém um heading h2 com o texto Page requested;', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/keuwe');
+
+  const takeText = screen.getByText(/page requested not found 😭/i);
+  expect(takeText).toBeInTheDocument();
+}); */
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/keuwe');
+
+  const takeImage = screen.getByRole('img',
+    { src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif' });
+  expect(takeImage).toBeInTheDocument();
+});
